56c23a8ff273fee427346d7814ec46322b991a1f
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-12-30 19:45:21 +0100

    complete xylophone app

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(sound: sender.currentTitle!)
+    }
+    
+    func playSound(sound:String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        
+    }
 }
-
